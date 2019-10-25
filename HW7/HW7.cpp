f4357ab57de8399c834e77a6666b70b0f585d45b
Grant Rydquist <gjr68@gjr68-1.mae.cornell.edu>
2019-10-25 10:50:22 -0400

    ostensibly finished

--- HW7/HW7.cpp
+++ HW7/HW7.cpp
@@ -20,9 +20,12 @@ Doub func(Doub x, Doub y){
 
 struct rhs {
     Int c=0;
+    Doub eps=1.0e-16;
     void operator() (const Doub x, VecDoub_I &y, VecDoub_O &dydx) {
+        eps = x;
+        if (x==0){eps=1.0e-16;};
         dydx[0]= y[1];
-        dydx[1]=-1/x*y[1]-(1-1/x/x)*y[0];
+        dydx[1]=-1/eps*y[1]-(1-1/eps/eps)*y[0];
         c++;
     }
 };
@@ -30,24 +33,75 @@ struct rhs {
 int main(){
 //======================P1=====================
 
-Doub j1 = Mybess.jn(1,1.0),j1p,j0 = Mybess.jn(0,1.0),atol = 1.0e-6,
-h1 = 0.01,x1 =1.0,x2 = 30.0,rtol = atol, j30 = Mybess.jn(1,30.0);
+Doub j1 = Mybess.jn(1,1.0),j1p,j0 = Mybess.jn(0,1.0),atol1 = 1.0e-6,
+h1 = 0.001,x1 =1.0,x2 = 30.0,rtol1 = atol1, j30 = Mybess.jn(1,30.0),
+rtol2 = 1.0e-10, atol2 = rtol2;
 j1p = j0 - j1;
 VecDoub yst(2);
 yst[0] = j1;
 yst[1] = j1p;
-Output out(20);
+Output out1(20),out2(20),out3(20),out4(20),out5(20);
 rhs d;
 
-Odeint<StepperDopr5<rhs> > myode(yst,x1,x2,atol,rtol,h1,0.0,out,d);
+
+Odeint<StepperDopr5<rhs> > myode(yst,x1,x2,atol1,rtol1,h1,0.0,out1,d);
 myode.integrate();
 
-cout << "For RK5, number of function evals: "<<d.c<<endl;
+cout <<endl<< "For RK5, 1e-6, number of function evals: "<<d.c<<endl;
 cout << "Error of J1(30): "<<
-abs(out.ysave[0][20]-j30)/j30<<endl;
+abs(out1.ysave[0][20]-j30)/abs(j30)<<endl<<endl;
 
 d.c = 0;
+yst[0] = j1;
+yst[1] = j1p;
 
-Odeint<StepperBS<rhs> > myode(yst,x1,x2,atol,rtol,h1,0.0,out,d);
+
+
+Odeint<StepperBS<rhs> > myode2(yst,x1,x2,atol1,rtol1,h1,0.0,out2,d);
+myode2.integrate();
+
+
+cout << "For BS, 1e-6, number of function evals: "<<d.c<<endl;
+cout << "Error of J1(30): "<<
+abs(out2.ysave[0][20]-j30)/abs(j30)<<endl<<endl;
+
+d.c = 0;
+yst[0] = j1;
+yst[1] = j1p;
+
+
+
+Odeint<StepperDopr5<rhs> > myode3(yst,x1,x2,atol2,rtol2,h1,0.0,out3,d);
+myode3.integrate();
+
+cout << "For RK5, 1e-10, number of function evals: "<<d.c<<endl;
+cout << "Error of J1(30): "<<
+abs(out3.ysave[0][20]-j30)/abs(j30)<<endl<<endl;
+
+d.c = 0;
+yst[0] = j1;
+yst[1] = j1p;
+
+
+Odeint<StepperBS<rhs> > myode4(yst,x1,x2,atol2,rtol2,h1,0.0,out4,d);
+myode4.integrate();
+
+cout << "For BS, 1e-10, number of function evals: "<<d.c<<endl;
+cout << "Error of J1(30): "<<
+abs(out4.ysave[0][20]-j30)/abs(j30)<<endl<<endl;
+
+
+//======================P2=====================
+yst[0] = 0.0;
+yst[1] = 0.5;
+x1 = 0.0;
+x2 = 1.0;
+Odeint<StepperBS<rhs> > myode5(yst,x1,x2,atol1,rtol1,h1,0.0,out5,d);
+myode5.integrate();
+
+cout << "For BS, 1e-10, number of function evals: "<<d.c<<endl;
+cout << "Error of J1(30): "<<
+abs(out5.ysave[0][20]-j1)/abs(j1)<<endl<<endl;
+
 
 };
