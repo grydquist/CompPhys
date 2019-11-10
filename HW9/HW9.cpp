9af887bba301fc99b88a963072e8974b5f5dbfd0
Nick <nk638@cornell.edu>
2019-11-10 18:03:18 -0500

    HW9 in progress

--- HW9/HW9.cpp
+++ HW9/HW9.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // rhs
 struct d {
     Int q;
-    d(Int qq) : q(qq){}; 
+    d(Int qq) : q(qq){};
     void operator() (const Doub x, VecDoub_I &y, VecDoub_O &dydx) {
         dydx[0]= y[1];
         dydx[1]=(2*q*cos(2*x)-y[2])*y[0];
@@ -26,6 +26,37 @@ Int N2 = 1;
 for (Int i=0;i<15;i++){
     for (Int j=0;j<15;j++){
     };
+}
+
+//******************************************************************************
+
+Int k = 20;
+Doub q = 5.;
+
+MatDoub a(k,k);
+
+for (Int i=0; i<k; i++){
+  for (Int j=0; j<k; j++){
+    a[i][j]=0.;
+    if (i==j){
+      a[i][j] = 4.*i*j;
+      if (j != k-1){
+        a[i][j+1] = q;
+      }
+      if (j != 0){
+        a[i][j-1] = q;
+      }
+    }
+
+    }
+  }
+
+
+Jacobi  jac(a);
+
+for (Int i=0; i<k; i++){
+  cout <<a[i][i+1] << endl;
+  cout << jac.d[i] << endl;
 }
 
 };
